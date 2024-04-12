ed2feec4b6ddc3220ef524a49da1f7aaa82e03fb
Ankit Kumar <105793993+ankitkumaro632@users.noreply.github.com>
2024-04-12 15:43:24 +0530

    remove multithreading code in master branch.

---
+++ src/main/java/com/java/learning/concept/SpringBootLearningConcept.java
@@ -0,0 +1,11 @@
+package com.java.learning.concept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningConcept {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearningConcept.class, args);
+    }
+}
--- src/test/java/com/java/learning/concept/SpringbootMultithreadingExampleApplicationTests.java
+++ src/test/java/com/java/learning/concept/SpringbootMultithreadingExampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javatechie.executor.api;
+package com.java.learning.concept;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
